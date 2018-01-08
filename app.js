107b343f002ef6e3729b939f2e65679cf58cdb14
Anne Starley <annestarley@gmail.com>
2018-01-08 13:51:40 -0800

    added nodemon, created ping-pong

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const listener = () => console.log(`Application listening on port ${port}!`)
+const listener = () => console.log(`Party on port ${port}! And different changes`)
 app.listen(port, listener)
+
+app.get('/ping', (req, res) => {
+  const message = 'pong!'
+  console.log(`ON THE SERVER: ${message}`)
+  res.send(`RESPONDING TO THE CLIENT: ${message}`)
+})
